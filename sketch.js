a18e4aa7e3b95de5532615689ec4f3eb6371cf70
kkr0913 <40028293+kkr0913@users.noreply.github.com>
2019-12-15 00:19:58 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,30 @@
+var gifLength = 2;
+var canvas;
+var img1, img2;
+var bool = true;
+
+function preload() {
+	img1 = loadImage('title1.png');
+	img2 = loadImage('title2.png');
+}
+
+function setup() {
+	var p5Canvas = createCanvas(950, 550);
+	canvas = p5Canvas.canvas;
+	imageMode(CENTER);
+	frameRate(2);
+	capturer.start();
+}
+
+function draw() {
+	if (bool) { image(img1, width/2, height/2); }
+	else { image(img2, width/2, height/2); }
+	bool = !bool;
+	
+	if (frameCount < gifLength) {
+		capturer.capture(canvas);
+	} else if (frameCount === gifLength) {
+		capturer.stop();
+		capturer.save();
+	}
+}
